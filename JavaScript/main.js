85c0fce69b590b4cfc660f0d3da529088c63d458
kmiloblanco <89950817+kmiloblanco@users.noreply.github.com>
2024-08-09 17:31:15 -0500

    Add files via upload

---
+++ JavaScript/main.js
@@ -0,0 +1,8 @@
+function contrasenaValida(contrasena) {
+    return contrasena === "2Fj(jjbFsuj" || contrasena === "eoZiugBf&g9";
+}
+
+console.log(contrasenaValida("2Fj(jjbFsuj"))
+console.log(contrasenaValida("eoZiugBf&g9"))
+console.log(contrasenaValida("hola"))
+console.log(contrasenaValida(""))
